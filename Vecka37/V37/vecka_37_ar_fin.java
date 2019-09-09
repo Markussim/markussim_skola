434bd2e08b68ba16a706557319247b6fa4aa0138
Markussim <markus.simonsen@gmail.com>
2019-09-09 12:49:07 +0200

    Lection 1

---
+++ Vecka37/V37/vecka_37_ar_fin.java
@@ -0,0 +1,23 @@
+package V37;
+
+public class vecka_37_ar_fin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Fin kommentar
+		/*
+		 * ksaöldldsö
+		 */
+		int helta1 = 5; // Fint
+		int svar; // Finare
+		svar = helta1*helta1;
+		System.out.println("Haha " + Math.pow(svar,svar));
+		
+		double deci = 3.14;
+		double radie = 5;
+		double area = deci*radie*radie;
+		System.out.println("FInt " + area);
+		
+	}
+
+}
--- src/anton/Anton_ar_dum.java
+++ src/anton/Anton_ar_dum.java
@@ -5,7 +5,6 @@ public class Anton_ar_dum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("haha");
-
 	}
 
 }
