c32e5f7005ffe371d983e8648e92f246051b1de6
Markussim <markus.simonsen@gmail.com>
2019-09-10 11:41:30 +0200

    Trying to fix some things

---
+++ Vecka37/V37/lektion2.java
@@ -0,0 +1,13 @@
+package V37;
+
+import java.util.Scanner;
+
+public class lektion2 {
+
+	public static void main(String[] args) {
+		String namn1 = "Markus Simonsen";
+		String namn2 = "Linus Pucko";
+		Scanner input = new Scanner(System.in);
+	}
+
+}
