df03e463246722b489fc6aa35f1331a4f7f6773e
Markussim <haha@haha.com>
2019-10-01 16:12:44 +0200

    Made countdown work

--- egna_saker/countdown/MainCountdown.java
+++ egna_saker/countdown/MainCountdown.java
@@ -15,46 +15,26 @@ public class MainCountdown {
 		String time = input.nextLine();
 
 		if (5 == time.length()) {
-
-			System.out.println(time.substring(0, 2)); //debug
-
-			System.out.println(time.substring(3)); //debug
-
 			if ((time.substring(3)).matches("[0-9]+") && ((time.substring(0, 2).matches("[0-9]+")))) {
 
-				System.out.println("Correct format"); //debug
-				
 				int timmar = Integer.parseInt(time.substring(0, 2));
-				
+
 				int minuter = Integer.parseInt(time.substring(3));
-				
+
 				int totalSec = timmar * 3600;
-				
+
 				totalSec = totalSec + minuter * 60;
 				
-				System.out.println(totalSec); //debug
-				
-			    LocalTime currentTime_local = java.time.LocalTime.now();
-			    
-			    String currentTime = currentTime_local.toString();
-			    
-			    System.out.println(currentTime); //debug
-			    
-			    int currentTimmar = Integer.parseInt(currentTime.substring(0, 2));
-			    
-			    System.out.println(currentTimmar); //debug
-			    
-			    System.out.println(currentTime.substring(3).substring(0, 2)); //debug
-			    
-			    System.out.println("waaaa: "+currentTime.substring(3)); //debug
-			    
-			    int currentMinuter = Integer.parseInt(currentTime.substring(3).substring(0, 2));
-			    
-			    int currentTotalSec = currentTimmar * 3600;
-			    
-			    currentTotalSec = currentTotalSec + currentMinuter * 60;
-			    
-			    System.out.println(currentTotalSec);
+				while (true ) {
+					the_actual_countdown(totalSec);
+					try {
+						Thread.sleep(10);
+					}
+					
+					catch (Exception e) {
+						// TODO: handle exception
+					}
+				}
 			}
 
 			else {
@@ -65,6 +45,28 @@ public class MainCountdown {
 
 			System.out.println("Please use format HH:MM");
 		}
+		input.close();
+	}
+
+	public static void the_actual_countdown(int totalSec) {
+
+		LocalTime currentTime_local = java.time.LocalTime.now();
+
+		String currentTime = currentTime_local.toString();
+
+		int currentTimmar = Integer.parseInt(currentTime.substring(0, 2));
+
+		int currentMinuter = Integer.parseInt(currentTime.substring(3).substring(0, 2));
+		
+		int currentSekunder = Integer.parseInt(currentTime.substring(6).substring(0, 2));
+
+		int currentTotalSec = currentTimmar * 3600;
+
+		currentTotalSec = currentSekunder + currentTotalSec + currentMinuter * 60;
+
+		int doneTime = totalSec - currentTotalSec;
+
+		System.out.println(doneTime);
 
 	}
 
