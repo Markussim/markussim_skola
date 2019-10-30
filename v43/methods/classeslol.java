792929079411add0ed6d8167e3d62f51415256fb
Markussim <markus.simonsen@gmail.com>
2019-10-30 11:49:45 +0100

    Continued with classes

--- v43/methods/classeslol.java
+++ v43/methods/classeslol.java
@@ -2,14 +2,7 @@ package methods;
 
 public class classeslol {
     public static void main(String[] args) {
-        testingClass testingObj = new testingClass();
-        System.out.println(testingObj.hahaMethod(100));
-    }
-}
-
-class testingClass {
-    public int hahaMethod(int haha) {
-        System.out.println("This prints something");
-        return haha;
+        //testingClass testingObj = new testingClass();
+        System.out.println(testingClass.hahaMethod(1000));
     }
 }
---
+++ v43/methods/testingClass.java
@@ -0,0 +1,8 @@
+package methods;
+
+public class testingClass {
+    public static int hahaMethod(int haha) {
+        System.out.println("This prints something");
+        return haha;
+    }
+}
