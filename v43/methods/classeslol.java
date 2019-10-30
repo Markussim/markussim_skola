c823a05b26750338224b608804febac69c92e052
Markussim <markus.simonsen@gmail.com>
2019-10-30 11:50:03 +0100

    Continued with classes

--- v43/methods/classeslol.java
+++ v43/methods/classeslol.java
@@ -4,5 +4,6 @@ public class classeslol {
     public static void main(String[] args) {
         //testingClass testingObj = new testingClass();
         System.out.println(testingClass.hahaMethod(1000));
+        //comment
     }
 }
