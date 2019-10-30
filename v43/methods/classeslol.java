5f60326b3e6480c10ba63b525239e07b1685770f
Markussim <markus.simonsen@gmail.com>
2019-10-30 10:34:56 +0100

    started making classes

---
+++ v43/methods/classeslol.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class classeslol {
+    public static void main(String[] args) {
+        testingClass testingObj = new testingClass();
+        System.out.println(testingObj.x);
+    }
+}
+
+class testingClass {
+   int x = 1;
+}
