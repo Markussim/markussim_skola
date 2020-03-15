563c88f73b4e34b1fd844c3cd3e0c8a8aaf40cac
Your Name <you@example.com>
2020-03-15 18:43:41 +0100

    Added some things to talspelet

--- v11/talspelet/Talspelet.java
+++ v11/talspelet/Talspelet.java
@@ -6,53 +6,81 @@ public class Talspelet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //Sets up a scanner
 
-        int high = 100; //Defines variables for the highest and lowest possible values
-        int low = 0;
+        boolean finnished = false;
 
-        int lives = 10; //Defines the number of lives, eg. how many times you can try
+        while (!finnished) {
 
-        int correct = (int) ((Math.random() * ((high - low) + 1)) + low); //Gets a random number that the user can later guess
+            int high = 100; //Defines varibles
+            int low = 0;
 
-        boolean isWinner = false; //This variable is used the stop the loop as it defines if the user has won yet
+            int lives = 10; //Defines the number of lives, eg. how many times you can try
 
-        while (!isWinner){ //This loop runs almost everything in the program that needs to be repeated, for example guessing a number
-
-            if (lives < 1) { //The first thing the loop does is to check if you have lost. Losing is having too few lives and tells you that you died and also tells you the number you tried to guess, but failed to guess
-                System.out.println("You died.");
-                System.out.println(correct);
-                System.exit(0); //It then exits the program. No need to play again if you're this bad at it
+            try {
+                System.out.println("Type highest and lowest numbers you'd like to guess in that order");
+                high = input.nextInt();
+                low = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("As you did not enter a correct number, default values of 0-100 have been set");
             }
 
-            System.out.println("Pick a number"); //This tells you to pick and number and tells you how many lives you have left
-            System.out.println("You have " + lives + " lives left");
-
-            int guess = 0; //This variable need to be something, even though this value is never used
-
-            try { //A try catch is used to prevent the program from crashing when a non-int value is entered
-                guess = input.nextInt(); //Gets a number from the user
-            } catch (Exception e) { //This runs if the scanner didn't succeed
-                System.out.println("Please enter a number"); //It tells you to enter a number as it can be expected that that is what went wrong
-                input.nextLine(); //Goes to the next line to prevent a loop
-                lives += -1; //Removes a life because you need to be punished
-                continue; //Goes back to the front of the loop
+            try {
+                System.out.println("Type how many lives you'd like");
+                lives = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("As you did not enter a correct number, default values of 10 lives have been set");
             }
 
-            if (guess > correct) { //If the guess is too high, it tells you so
-                System.out.println("Guess is too high");
-                lives--; //It removes a life because you need to be punished
-                continue;
-            }
+            int correct = (int) ((Math.random() * ((high - low) + 1)) + low); //Gets a random number that the user can later guess
 
-            else if (guess < correct) { //If the guess is too low, it tells you so
-                System.out.println("Guess is too low");
-                lives--; //It removes a life because you need to be punished
-                continue;
-            }
+            boolean isWinner = false; //This variable is used the stop the loop as it defines if the user has won yet
 
-            else { //If it isn't too low or too high it has to be correct, so it makes the variable true
-                isWinner = true;
-            }
+            while (!isWinner){ //This loop runs almost everything in the program that needs to be repeated, for example guessing a number
+
+                if (lives < 1) { //The first thing the loop does is to check if you have lost. Losing is having too few lives and tells you that you died and also tells you the number you tried to guess, but failed to guess
+                    System.out.println("You died.");
+                    System.out.println(correct);
+                    System.exit(0); //It then exits the program. No need to play again if you're this bad at it
+                }
+
+                System.out.println("Pick a number"); //This tells you to pick and number and tells you how many lives you have left
+                System.out.println("You have " + lives + " lives left");
+
+                int guess = 0; //This variable need to be something, even though this value is never used
+
+                try { //A try catch is used to prevent the program from crashing when a non-int value is entered
+                    guess = input.nextInt(); //Gets a number from the user
+                } catch (Exception e) { //This runs if the scanner didn't succeed
+                    System.out.println("Please enter a number"); //It tells you to enter a number as it can be expected that that is what went wrong
+                    input.nextLine(); //Goes to the next line to prevent a loop
+                    lives += -1; //Removes a life because you need to be punished
+                    continue; //Goes back to the front of the loop
+                }
+
+                if (guess > correct) { //If the guess is too high, it tells you so
+                    System.out.println("Guess is too high");
+                    lives--; //It removes a life because you need to be punished
+                    continue;
+                }
+
+                else if (guess < correct) { //If the guess is too low, it tells you so
+                    System.out.println("Guess is too low");
+                    lives--; //It removes a life because you need to be punished
+                    continue;
+                }
+
+                else { //If it isn't too low or too high it has to be correct, so it makes the variable true
+                    System.out.println("You have won, would you like to play again? Type yes to play again, or anything else for no");
+                    input.nextLine(); //Takes input
+                    String play_again = input.nextLine(); //Sets string to input
+                    if (play_again.contains("yes")) { //If it contains yes, play the game again
+                        finnished = false;
+                    }
+                    else {
+                        finnished = true;
+                    }
+                    isWinner = true;
+                }
         }
-        System.out.println("Correct number!"); //As the loop is now finished, it runs this
+        }
     }
 }
