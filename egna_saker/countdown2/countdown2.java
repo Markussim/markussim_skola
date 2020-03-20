f4c623f82bdb52789677746eba6e2002f7d5a5b3
Your Name <you@example.com>
2020-03-20 19:03:57 +0100

    Added some comments to countdown2

--- egna_saker/countdown2/countdown2.java
+++ egna_saker/countdown2/countdown2.java
@@ -14,19 +14,22 @@ public class countdown2 {
         }
 
         while (true) {
+            System.out.println(LocalTime.now().until(hoursAndMinutes, ChronoUnit.MINUTES)); //This is where it actually counts down, it simply uses the method included in LocalTime for that
             System.out.println(LocalTime.now().until(hoursAndMinutes, ChronoUnit.SECONDS));
         }
     }
 
-    public static LocalTime setLocalTime(String someTextWithTime) {
+    public static LocalTime setLocalTime(String someTextWithTime) { //This method takes a string and turns it into a LocalTime object
 
-        int hours = Integer.parseInt(someTextWithTime.substring(0, 2));
-        int minutes = Integer.parseInt(someTextWithTime.substring((someTextWithTime.length() - 2)));
+        int hours = Integer.parseInt(someTextWithTime.substring(0, 2)); //This gets the characters between positions 0 and 2 and turns it into an int
+        int minutes = Integer.parseInt(someTextWithTime.substring((someTextWithTime.length() - 2))); //This gets the last two characters and turns it into an int
 
         LocalTime hoursAndMinutes = LocalTime.of(hours, minutes); //Sets hours and minutes to an object in the LocalTime class
 
         return hoursAndMinutes;
     }
+
+
     public static String get_a_string() { //Get input from user and turns it into a string
         Scanner input = new Scanner(System.in); //Initialises a scanner
         String input_string;
